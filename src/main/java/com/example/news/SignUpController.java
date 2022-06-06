bed42a1edc1e0690ee3af6df0cef62724ada363c
KODlya <kurt_olg@mail.ru>
2022-06-07 00:38:17 +0700

    hui

---
+++ src/main/java/com/example/news/SignUpController.java
@@ -0,0 +1,45 @@
+package com.example.news;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class SignUpController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private RadioButton AdminRadioButton;
+
+    @FXML
+    private RadioButton RadioButton;
+
+    @FXML
+    private Button SignUpLoginButton;
+
+    @FXML
+    private PasswordField SignUpPassword;
+
+    @FXML
+    private TextField signUpLogin;
+
+    @FXML
+    private ToggleGroup tgl1;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
